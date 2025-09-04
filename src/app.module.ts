f497b6adf5fa9aa315aa26371150472fdcc83fd9
martingaidoo <martingaido00@gmail.com>
2025-09-04 15:41:51 -0300

    se agrega la configuracion de la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
-
 import { AppService } from './app.service';
 import { ImcModule } from './module/imc/imc.module';
 import { AppController } from './app.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ImcModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'rootpassword',
+      database: 'imc_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ImcModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
