c3d0796052e6c2fcfe7246c978f7fd5cadd6d383
MartinCesano <cesano305@gmail.com>
2025-09-12 18:48:18 +0000

    Se agregaron validaciones y mensajes de error descriptivos al metodo de calcular

--- src/module/imc/dto/calcular-imc-dto.ts
+++ src/module/imc/dto/calcular-imc-dto.ts
@@ -1,11 +1,14 @@
-import { IsNumber, Min } from 'class-validator';
+import { IsNumber, Max, Min } from 'class-validator';
 
 export class CalcularImcDto {
   @IsNumber()
-  @Min(0.1) // Altura mínima razonable
+  @Min(0.1, { message: 'La altura debe ser mayor a 0.1 metros' }) // Altura mínima razonable
+  @Max(3, { message: 'La altura debe ser menor a 3 metros' }) // Altura máxima razonable
   altura: number;
 
   @IsNumber()
-  @Min(1) // Peso mínimo razonable
+  @Min(0.1, { message: 'El peso debe ser mayor a 0.1 kilogramos' }) // Peso mínimo razonable
+  @Max(500, { message: 'El peso debe ser menor a 500 kilogramos' }) // Peso máximo razonable
   peso: number;
 }
+  
