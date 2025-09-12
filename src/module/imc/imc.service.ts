3f1c79fdd68145c6781a618dbe1a1a38e0011cfe
Franco <francovalentinferraro@outlook.com>
2025-09-12 16:45:33 -0300

    Configuraciones de jest

--- src/module/imc/imc.service.ts
+++ src/module/imc/imc.service.ts
@@ -1,11 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CalcularImcDto } from "./dto/calcular-imc-dto";
 
 
 @Injectable()
 export class ImcService {
   calcularImc(data: CalcularImcDto): { imc: number; categoria: string } {
+    
     const { altura, peso } = data;
+    if (peso <= 0 || peso >= 500) {
+      throw new BadRequestException("El peso debe ser mayor a 0 y menor a 500 kg");
+    }
+    if (altura <= 0 || altura >= 3) {
+      throw new BadRequestException("La altura debe ser mayor a 0 y menor a 3 metros");
+    }
     const imc = peso / (altura * altura);
     const imcRedondeado = Math.round(imc * 100) / 100; // Dos decimales
 
@@ -19,6 +26,7 @@ export class ImcService {
     } else {
       categoria = 'Obeso';
     }
+    console.log({ altura, peso, imc });
 
     return { imc: imcRedondeado, categoria };
   }
