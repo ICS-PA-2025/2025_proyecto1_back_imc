366130ec6c3ae5ab3e86992a486f03b212a5fa3f
Franco <francovalentinferraro@outlook.com>
2025-09-14 21:45:56 -0300

    Inclusion de tests de integracion IMC para las validaciones de categoria

--- src/module/imc/imc.service.spec.ts
+++ src/module/imc/imc.service.spec.ts
@@ -6,10 +6,12 @@ import { BadRequestException } from '@nestjs/common';
 
 describe('ImcService', () => {
   let service: ImcService;
+  let mockImcRepository: any;
 
   beforeEach(async () => {
-    const mockImcRepository = {
+    mockImcRepository = {
       create: jest.fn(),
+      findAll: jest.fn(),
     };
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -29,7 +31,7 @@ describe('ImcService', () => {
   });
 
   describe('calcularImc', () => {
-    // ✅ Casos válidos
+    // Casos válidos
     it('debería calcular un IMC normal', () => {
       const dto: CalcularImcDto = { altura: 1.75, peso: 70 };
       const result = service.calcularImc(dto);
@@ -58,7 +60,7 @@ describe('ImcService', () => {
       expect(result.categoria).toBe('Obeso');
     });
 
-    // ⚠️ Casos inválidos (validaciones)
+    // Casos inválidos (validaciones)
     it('debería lanzar error si el peso es <= 0', () => {
       const dto: CalcularImcDto = { altura: 1.70, peso: 0 };
       expect(() => service.calcularImc(dto)).toThrow(
@@ -86,5 +88,78 @@ describe('ImcService', () => {
         new BadRequestException('La altura debe ser mayor a 0 y menor a 3 metros'),
       );
     });
+  });
+
+  describe('findAll', () => {
+    it('debería retornar el historial de IMC mapeado correctamente', async () => {
+      const mockImcList = [
+        {
+          id: 1,
+          peso: 70,
+          altura: 1.75,
+          imc: 22.86,
+          imcRedondeado: 22.86,
+          categoria: 'Normal',
+          fechahora: new Date('2025-09-12T10:00:00Z'),
+        },
+        {
+          id: 2,
+          peso: 80,
+          altura: 1.8,
+          imc: 24.69,
+          imcRedondeado: 24.69,
+          categoria: 'Normal',
+          fechahora: new Date('2025-09-11T10:00:00Z'),
+        },
+      ];
+      mockImcRepository.findAll.mockResolvedValue(mockImcList);
+
+      const result = await service.findAll();
+      expect(result).toEqual([
+        {
+          id: 1,
+          peso: 70,
+          altura: 1.75,
+          imc: 22.86,
+          imcRedondeado: 22.86,
+          categoria: 'Normal',
+          fechahora: new Date('2025-09-12T10:00:00Z'),
+        },
+        {
+          id: 2,
+          peso: 80,
+          altura: 1.8,
+          imc: 24.69,
+          imcRedondeado: 24.69,
+          categoria: 'Normal',
+          fechahora: new Date('2025-09-11T10:00:00Z'),
+        },
+      ]);
+      expect(mockImcRepository.findAll).toHaveBeenCalledWith(undefined, undefined);
+    });
+  });
+  // Tests integrales para probar las catergorias de IMC
+    it('debería devolver "Bajo peso" si IMC < 18.5', () => {
+    const result = service.calcularImc({ peso: 50, altura: 1.80 }); // IMC ~15.43
+    expect(result).toEqual({ imc: 15.43, categoria: 'Bajo peso' });
+    expect(repository.create).toHaveBeenCalled();
+  });
+
+  it('debería devolver "Normal" si 18.5 ≤ IMC < 25', () => {
+    const result = service.calcularImc({ peso: 70, altura: 1.75 }); // IMC ~22.86
+    expect(result).toEqual({ imc: 22.86, categoria: 'Normal' });
+    expect(repository.create).toHaveBeenCalled();
+  });
+
+  it('debería devolver "Sobrepeso" si 25 ≤ IMC < 30', () => {
+    const result = service.calcularImc({ peso: 85, altura: 1.75 }); // IMC ~27.76
+    expect(result).toEqual({ imc: 27.76, categoria: 'Sobrepeso' });
+    expect(repository.create).toHaveBeenCalled();
+  });
+
+  it('debería devolver "Obeso" si IMC ≥ 30', () => {
+    const result = service.calcularImc({ peso: 110, altura: 1.75 }); // IMC ~35.92
+    expect(result).toEqual({ imc: 35.92, categoria: 'Obeso' });
+    expect(repository.create).toHaveBeenCalled();
   });
 });
