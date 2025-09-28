98298a9b68b0555ad2ff64d825b05e78175a50eb
MartinCesano <cesano305@gmail.com>
2025-09-28 15:04:38 +0000

    Mejora de legibilidad en el archivo mongo.config

--- src/database/mongo.config.ts
+++ src/database/mongo.config.ts
@@ -3,9 +3,16 @@ import { ConfigService } from '@nestjs/config';
 
 export const mongooseAsyncConfig: MongooseModuleAsyncOptions = {
   inject: [ConfigService],
-  useFactory: (config: ConfigService) => ({
-    uri:
-      config.get<string>('MONGO_URI') ||
-      `mongodb://${config.get<string>('MONGO_HOST', 'localhost')}:${config.get<number>('MONGO_PORT', 27017)}/${config.get<string>('MONGO_DATABASE', 'imc_db')}`,
-  }),
+  useFactory: (config: ConfigService) => {
+    // Obtener variables de entorno
+    const mongoUri = config.get<string>('MONGO_URI');
+    const mongoHost = config.get<string>('MONGO_HOST', 'localhost');
+    const mongoPort = config.get<number>('MONGO_PORT', 27017);
+    const mongoDatabase = config.get<string>('MONGO_DATABASE', 'imc_db');
+
+    // Construir URI si no existe una completa
+    const uri = mongoUri || `mongodb://${mongoHost}:${mongoPort}/${mongoDatabase}`;
+
+    return { uri };
+  },
 };
